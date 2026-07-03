b021c02c2feb734ce1176d9b69844b2120ebc779
AkhilaVarma18 <akhilavarmaa18@gmail.com>
2026-07-03 13:22:37 +0530

    firstcommit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
 import { Routes } from '@angular/router';
+import { CategoryList } from './features/category/category-list/category-list';
+import { AddCategory } from './features/category/add-category/add-category';
+import { EditCategory } from './features/category/edit-category/edit-category';
+import { EditBlogpost } from './features/blogpost/edit-blogpost/edit-blogpost';
+import { AddBlogpost } from './features/blogpost/add-blogpost/add-blogpost';
+import { BlogpostList } from './features/blogpost/blogpost-list/blogpost-list';
+import { Home } from './features/public/home/home';
+import { BlogDetails } from './features/public/blog-details/blog-details';
+import { Login } from './features/auth/login/login';
+import { guardsGuard } from './features/auth/guards-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+        path: 'admin/categories',
+        component: CategoryList,
+        canActivate: [guardsGuard]
+    },
+    {
+        path:'admin/categories/add',
+        component: AddCategory,
+        canActivate: [guardsGuard]
+    },
+    {
+        path:'admin/categories/edit/:id',
+        component: EditCategory,
+        canActivate: [guardsGuard]
+    },
+     {
+        path: 'admin/blogposts',
+        component: BlogpostList,
+        canActivate: [guardsGuard]
+    },
+     {
+        path: 'admin/blogposts/add',
+        component: AddBlogpost,
+        canActivate: [guardsGuard]
+    },
+    {
+        path:'admin/blogposts/edit/:id',
+        component: EditBlogpost,
+        canActivate: [guardsGuard]
+    },
+    {
+          path:'',
+        component:Home
+
+    },
+    {
+        path:'blog/:url',
+        component:BlogDetails
+    }
+    ,{
+        path:'login',
+        component: Login
+    }
+
+];
---
+++ src/app/features/category/category-list/category-list.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CategoryService } from '../services/category-service';
+
+@Component({
+  selector: 'app-category-list',
+  imports: [RouterLink],
+  templateUrl: './category-list.html',
+  styleUrl: './category-list.css',
+})
+export class CategoryList {
+  private categoryService=inject(CategoryService);
+  private getallCategoriesRef=this.categoryService.getCategory();
+  isLoading=this.getallCategoriesRef.isLoading;
+   isError=this.getallCategoriesRef.error;
+   value=this.getallCategoriesRef.value;
+}
